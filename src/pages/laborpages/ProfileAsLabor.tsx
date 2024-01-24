7e635a932dcd79cc8def88c121f5ded6a9b5df7d
Saurabh Anand <saurabhbebi@gmail.com>
2024-01-24 18:30:22 +0530

    update

--- src/pages/laborpages/ProfileAsLabor.tsx
+++ src/pages/laborpages/ProfileAsLabor.tsx
@@ -188,7 +188,7 @@ function ProfileAsLabor() {
                           onChange={handleImageChange}
                         />
                       ) : (
-                        <Avatar className='shadow-[inset_-1px_1px_1px_1px_#48bb78,inset_1px_-1px_1px_1px_#48bb78] h-24 w-24'>
+                        <Avatar className='w-24 h-24 '>
                           <AvatarImage src={userLabor?.profileImage || fallbackImages.default} />
                           <AvatarFallback>{userLabor?.name[0]}</AvatarFallback>
                         </Avatar>
