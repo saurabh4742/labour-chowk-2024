220f778c64a46b39c4ef88c0f9cb4965ba795683
Saurabh Anand <saurabhbebi@gmail.com>
2024-01-26 21:03:17 +0530

    update

--- src/pages/employerpages/EmployerHome.tsx
+++ src/pages/employerpages/EmployerHome.tsx
@@ -36,7 +36,7 @@ function EmployerHome() {
               </Avatar>
           </div>
             <Label className="flex justify-center w-full mx-auto mt-4 text-2xl text-center sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
-              Labor Chowk welcomes you<br/>Mr./Mrs. {userEmployer?.name} <PartyPopper />!
+              Labor Chowk welcomes you<br/>Mr./Mrs. {userEmployer.name} <PartyPopper />!
             </Label>
           {searchQuery ? (
             <LaborsCard
