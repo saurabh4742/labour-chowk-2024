c7492d15e7dd5fc01197894abfc13ebe0dcc8940
Saurabh Anand <saurabhbebi@gmail.com>
2024-01-26 21:45:08 +0530

    update

--- src/pages/employerpages/EmployerHome.tsx
+++ src/pages/employerpages/EmployerHome.tsx
@@ -19,7 +19,7 @@ function EmployerHome() {
     <div>
       {userEmployer ? (
         <>
-        <Label className="flex justify-center w-full mx-auto mt-6 text-lg text-center sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
+        <Label className="flex justify-center w-full mx-auto mt-6 text-xl text-center sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
         Certified<Fingerprint className="mx-2 " />kaamgaar dhundhe<SearchCheck className="mx-2 "/>!
           </Label>
           <div className="flex justify-center w-full mt-4">
--- src/pages/laborpages/LaborPortal.tsx
+++ src/pages/laborpages/LaborPortal.tsx
@@ -18,7 +18,7 @@ function LaborPortal() {
               <AvatarFallback>LC</AvatarFallback>
             </Avatar>
           </div>
-          <Label className="flex justify-center w-full mx-auto mt-4 text-2xl text-center sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
+          <Label className="flex justify-center w-full mx-auto mt-4 text-lg text-center sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
             Labor Chowk welcomes you
             <br />
             Mr./Mrs. {userLabor.name}!
