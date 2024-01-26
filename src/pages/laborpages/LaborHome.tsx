353807b0a96795a4af05ccc506f59b967315d355
Saurabh Anand <saurabhbebi@gmail.com>
2024-01-26 23:21:37 +0530

    update

--- src/pages/laborpages/LaborHome.tsx
+++ src/pages/laborpages/LaborHome.tsx
@@ -20,9 +20,7 @@ function LaborHome() {
       {userLabor ? (
         <>
           <Label className="flex justify-center w-full mx-auto mt-6 text-xl text-center sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
-            Authentic
-            <ShieldCheck color="green" className="mx-2" />
-            job opportunities dhundhe!
+            Authentic<ShieldCheck color="green" className="mx-2" />job opportunities dhundhe!
           </Label>
           <div className="flex justify-center w-full mt-4">
             <Input
