ce3e2a93d411325fe8de60502bbb4988e94c7c45
Saurabh Anand <saurabhbebi@gmail.com>
2024-01-26 20:55:30 +0530

    update

--- src/pages/laborpages/LaborHome.tsx
+++ src/pages/laborpages/LaborHome.tsx
@@ -33,7 +33,6 @@ function LaborHome() {
               <AvatarFallback>LC</AvatarFallback>
             </Avatar>
           </div>
-          
             <Label className="flex justify-center w-full mx-auto mt-4 text-2xl sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
               Labor Chowk welcomes you<br/>Mr./Mrs. {userLabor?.name}!
             </Label>
